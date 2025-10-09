87fc275eb20b8aab6a3d6289bde39ea4ecc39384
thefinik <thefinik@gmail.com>
2025-10-09 17:21:40 +0100

    01_basics: Python fundamentals (variables, loops, functions)

---
+++ 01_basics/functions.py
@@ -0,0 +1,5 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+message = greet("Andrew")
+print(message)
---
+++ 01_basics/loops.py
@@ -0,0 +1,6 @@
+for i in range(1, 6):
+    print("Number:", i)
+
+while count < 3:
+    print("Count:", count)
+    count += 1
